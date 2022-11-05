f50cae7c655d1e2b3e0df4d8b2f1e0b41b18ad6d
SubaRya <index20010928@gmail.com>
2022-11-06 01:40:20 +0800

    feat: add getEntryInfo by json

---
+++ getBuildingEntryInfo.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+class Entry:
+    facing :str
+    pos: tuple
+    def __init__(self):
+        self.facing = ""
+        self.pos = tuple()
+
+class BuildingInfo:
+    entriesNameList = []
+    entriesInfo = {}
+    def __init__(self, filename: str= ""):
+        with open(filename, "r") as f:
+            fileData = json.load(f)
+            for entry in fileData["Entries"]:
+                entryData = Entry()
+                entryData.facing = entry["facing"]
+                entryData.pos = tuple(entry["roadStartPosition"])
+                self.entriesNameList.append(entry["type"])
+                self.entriesInfo[entry["type"]] = entryData
+    def getBuildingNameList(self) -> list: 
+        # for i in self.entriesNameList:
+        #     print("Entry Name: ", i)
+        return self.entriesNameList
+    def getEntryInfo(self, name: str) -> Entry:
+        # print("Entry facing:", self.entriesInfo[name].facing)
+        # print("Entry position:", self.entriesInfo[name].pos)
+        return self.entriesInfo[name]
+
+if __name__ == '__main__':
+    target = os.path.abspath("./data/structures/modern_house/modernHouseInfo.json")
+    buildingInfo = BuildingInfo(target)
+    List = buildingInfo.getBuildingNameList()
+    Info = buildingInfo.getEntryInfo("mainEntry")
