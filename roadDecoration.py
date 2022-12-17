b818d4e6a25082dca6548544dc11d9cb8cf16560
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2022-12-17 22:06:45 +0800

    feat: add tree decoration function

--- roadDecoration.py
+++ roadDecoration.py
@@ -16,20 +16,40 @@ flowers = [
     "sunflower"
 ]
 
-flowerStampSize = [[3, 3], [5, 8], [8, 20]]
-
+flowerStampSize = [[8, 3], [12, 5], [16, 2]]
+treeStampSize = [4, 3]
 
 # return value is [x,z,flowerName]
+
+
 def roadDecoration(roadCoord, interval, heights) -> []:
     flowerData = []
     size = len(roadCoord)
     for idx in range(0, size, interval):
         [x, y, z] = roadCoord[idx]
         [size, count] = choice(flowerStampSize)
+        flower = choice(flowers)
         for _ in range(count):
-            nx = randint(x, x+size)
-            nz = randint(z, z+size)
+            nx = randint(x - size, x+size)
+            nz = randint(z - size, z+size)
             flowerData.append(
-                [nx, heights[nx][nz], nz, choice(flowers)])
+                [nx, heights[nx][nz], nz, flower])
 
     return flowerData
+
+
+def treeDecoration(roadCoord, interval, heights) -> []:
+    treeData = []
+    size = len(roadCoord)
+    for idx in range(0, size, interval):
+        [x, y, z] = roadCoord[idx]
+        [size, count] = treeStampSize
+        for _ in range(count):
+            nx = randint(x - size, x+size)
+            nz = randint(z - size, z+size)
+            if len([x for x in roadCoord if (x[0] == nx and x[2] == nz)]) > 0:
+                continue
+            treeData.append(
+                [nx, heights[nx][nz], nz])
+
+    return treeData
