d29a17bb0422c180ad776bcdddcede58e4f0c668
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2023-04-20 11:24:54 +0800

    feat: add core initial logic
    Add core constructor
    Add method to get map data
    Add method to get different height data

--- interface/core.py
+++ interface/core.py
@@ -1,17 +1,38 @@
-from gdpc.vector_tools import Rect
+from gdpc import Editor
+from gdpc.vector_tools import Rect, Box
+from typing import Literal
 import numpy as np
 
+from heightinfo import HeightInfo
+from resource.AnalyzeAreaBiomeList import getAllBiomeList
+
+DEFAULT_BUILD_AREA = Box((0, 0, 0), (255, 255, 255))
+
 class Core():
-    def __init__(self,bound: Rect, heightMap, liquidMap, roadMap, sdMap, biomeMap, resources) -> None:
-        gameMap = {}
-        gameMap["height"] = heightMap
-        gameMap["liquid"] = liquidMap
-        gameMap["road"] = roadMap
-        gameMap["SD"] = sdMap
-        gameMap["biome"] = biomeMap
-        self.gameMap = gameMap
-        self.resources =  resources
-        self.blueprint = np.array(bound.size) # what should type of the blueprint be?
+    def __init__(self, buildArea=DEFAULT_BUILD_AREA) -> None:
+        """
+        the core will connect with the game
+        """
+        # initalize editor
+        editor = Editor(buffering=True, caching=True)
+        editor.setBuildArea(buildArea)
+        # get world slice and height maps
+        worldSlice = editor.loadWorldSlice(buildArea.toRect(), cache=True)
+        heights = worldSlice.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
+
+        # get top left and bottom right coordnidate
+        x1, _, x2 = buildArea.offset
+        z1, _, z2 = buildArea.offset + buildArea.size
+        x, _, z = buildArea.size
+
+        # To be discussed: Do we keep the class or save it into a 2d array
+        gameData = {}
+        gameData["biomes"] = getAllBiomeList(worldSlice, buildArea)
+        self.editor = editor
+        self.gameData = gameData
+        self.heightInfo = HeightInfo((x1, z1, x2, z2), heights) # contains: height, sd, var, mean
+        self.resources = {} 
+        self.blueprint = np.ndarray((x,z)) 
 
     def getResource(self, type: str): # Do we specify the boundary here?
         """Get the amount of a certain resource. If the resource does not exist, raise Exception"""
@@ -24,15 +45,26 @@ class Core():
         
         return True
 
-    def canBuild(self, bound: Rect) -> bool:
-        """Check if an area can build a new building"""
-        return True
-
-    def getMap(self, type: str, bound: Rect):
+    def getMap(self, type: str, bound: Rect = DEFAULT_BUILD_AREA.toRect()):
         if type in self.gameMap:
             return self.gameMap[type] # use the bound to get the map, need to check with lovesnow
         raise Exception(f"The requested map: {type}, does not exist in the core.")
 
+    def getHeight(self, heightType: Literal["var" , "mean" , "sum" , "squareSum"], bound: Rect):
+        x1, x2 = bound.offset
+        z1, z2 = bound.offset + bound.size
+        area = (x1, z1, x2, z2)
+        if heightType == "var":
+            return self.heightInfo.varArea(area)
+        if heightType == "mean":
+            return self.heightInfo.meanArea(area)
+        if heightType == "sum":
+            return self.heightInfo.sumArea(area)
+        if heightType == "squareSum":
+            return self.heightInfo.squareSumArea(area)
+        raise Exception("This type does not exist on heightType")
+
     def startBuildingInMinecraft(self):
         """Send the blueprint to Minecraft"""
         pass
+
