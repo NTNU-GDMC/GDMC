72d4bdafe08f71043309ea64e6a1ed3416f5f878
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2023-04-24 20:06:05 +0800

    refactor: use getter to get datas

--- interface/core.py
+++ interface/core.py
@@ -2,9 +2,12 @@ from gdpc import Editor
 from gdpc.vector_tools import Rect, Box
 from typing import Literal
 import numpy as np
+from building import Building
 
 from heightinfo import HeightInfo
 from resource.AnalyzeAreaBiomeList import getAllBiomeList
+
+from resource.AnalyzeAreaMaterial import analyzeSettlementMaterial
 
 DEFAULT_BUILD_AREA = Box((0, 0, 0), (255, 255, 255))
 
@@ -25,46 +28,61 @@ class Core():
         z1, _, z2 = buildArea.offset + buildArea.size
         x, _, z = buildArea.size
 
-        # To be discussed: Do we keep the class or save it into a 2d array
-        gameData = {}
-        gameData["biomes"] = getAllBiomeList(worldSlice, buildArea)
-        self.editor = editor
-        self.gameData = gameData
-        self.heightInfo = HeightInfo((x1, z1, x2, z2), heights) # contains: height, sd, var, mean
-        self.resources = {} 
-        self.blueprint = np.ndarray((x,z)) 
+        self._roadMap = np.ndarray((x,z))
+        self._liquidMap = worldSlice.heightmaps["OCEAN_FLOOR"] # TODO: Check what it does
+        self._biomeList = getAllBiomeList(worldSlice, buildArea)
+        self._editor = editor
+        self._resources = analyzeSettlementMaterial(worldSlice, buildArea)
+        self._heightInfo = HeightInfo((x1, z1, x2, z2), heights) # contains: height, sd, var, mean
+        self._blueprint = np.zeros((x // 2,z // 2)) # unit is 2x2
+        self._blueprintData = dict[int, Building]
 
-    def getResource(self, type: str): # Do we specify the boundary here?
-        """Get the amount of a certain resource. If the resource does not exist, raise Exception"""
-        if type in self.resources:
-            return self.resources[type]
-        raise Exception(f"The type: {type}, does not exist in the core.")
+    @property
+    def roadMap(self):
+        return self._roadMap
 
-    def addBuilding(self, pos, building, resourcesNeed) -> bool:
-        """Append a building on to the blueprint. Return False if it fails to add the building onto the blueprint"""
-        
-        return True
+    @property
+    def liquidMap(self):
+        return self._liquidMap
 
-    def getMap(self, type: str, bound: Rect = DEFAULT_BUILD_AREA.toRect()):
-        if type in self.gameMap:
-            return self.gameMap[type] # use the bound to get the map, need to check with lovesnow
-        raise Exception(f"The requested map: {type}, does not exist in the core.")
+    @property
+    def biomeList(self):
+        return self._biomeList
 
-    def getHeight(self, heightType: Literal["var" , "mean" , "sum" , "squareSum"], bound: Rect):
+    @property
+    def resources(self):
+        return self._resources
+
+    @property
+    def blueprint(self):
+        return self._blueprint
+
+    def getBlueprintBuildingData(self, id: int):
+        return self._blueprintData[id]
+    
+    def addBuilding(self, building: Building):
+        """Append a building on to the blueprint. We trust our agent, if there's any overlap, it's agent's fault."""
+        (x, z) = building.position
+        (xlen, zlen) = building.offset
+        id = len(self._blueprintData) + 1
+
+        self._blueprintData[id] = building
+        self._blueprint[x:x+xlen, z:z+zlen] = id
+
+    def getHeightMap(self, heightType: Literal["var" , "mean" , "sum" , "squareSum"], bound: Rect):
         x1, x2 = bound.offset
         z1, z2 = bound.offset + bound.size
         area = (x1, z1, x2, z2)
         if heightType == "var":
-            return self.heightInfo.varArea(area)
+            return self._heightInfo.varArea(area)
         if heightType == "mean":
-            return self.heightInfo.meanArea(area)
+            return self._heightInfo.meanArea(area)
         if heightType == "sum":
-            return self.heightInfo.sumArea(area)
+            return self._heightInfo.sumArea(area)
         if heightType == "squareSum":
-            return self.heightInfo.squareSumArea(area)
+            return self._heightInfo.squareSumArea(area)
         raise Exception("This type does not exist on heightType")
 
     def startBuildingInMinecraft(self):
         """Send the blueprint to Minecraft"""
         pass
-
