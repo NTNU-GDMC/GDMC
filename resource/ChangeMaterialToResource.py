e461a364a4f59f431062e5116e95f805eb0fbd86
SubaRya <index20010928@gmail.com>
2023-04-25 21:11:54 +0800

    fix: remove bedrock from initial material and update the rule of material to stone

--- resource/ChangeMaterialToResource.py
+++ resource/ChangeMaterialToResource.py
@@ -1,6 +1,6 @@
 # for stone, log, food, iron "no amount of human"
 # fix: add human transform - SubaRya
-stoneList = ["minecraft:andesite","minecraft:basalt","minecraft:cobblestone","minecraft:mossy_cobblestone","minecraft:mossy_stone_bricks","minecraft:cracked_stone_bricks","minecraft:diorite","minecraft:dripstone_block","minecraft:stone","minecraft:stone_bricks","minecraft:granite","minecraft:deepslate","minecraft:deepslate_bricks","minecraft:cobbled_deepslate","minecraft:tuff","minecraft:bedrock"]
+stoneList = ["minecraft:andesite","minecraft:basalt","minecraft:cobblestone","minecraft:mossy_cobblestone","minecraft:mossy_stone_bricks","minecraft:cracked_stone_bricks","minecraft:diorite","minecraft:dripstone_block","minecraft:stone","minecraft:stone_bricks","minecraft:granite","minecraft:deepslate","minecraft:deepslate_bricks","minecraft:cobbled_deepslate","minecraft:tuff"]
 logList = ["minecraft:oak_log","minecraft:dark_oak_log","minecraft:birch_log","minecraft:spruce_log","minecraft:jungle_log","minecraft:acacia_log","minecraft:stripped_oak_log","minecraft:stripped_birch_log","minecraft:stripped_spruce_log","minecraft:stripped_jungle_log","minecraft:stripped_acacia_log","minecraft:stripped_dark_oak_log","minecraft:mangrove_log"]
 ironList = ["minecraft:iron_ore","minecraft:raw_iron_block","minecraft:deepslate_iron_ore"]
 
@@ -17,6 +17,7 @@ def changeMaterialToResource(materialList):
     for idx in materialList:
         if idx in stoneList:
             stoneNum += materialList[idx]
+    stoneNum /= 10
     for idx in materialList:
         if idx in ironList:
             ironOreNum += materialList[idx]
