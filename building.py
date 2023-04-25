2aaad72084fe8a96ce5c01d6d2ae7e79324f8b0b
SubaRya <index20010928@gmail.com>
2023-04-26 00:38:03 +0800

    update: building class type

--- building.py
+++ building.py
@@ -1,8 +1,11 @@
 class Building(object):
-    def __init__(self, position: tuple[int, int], length: int, width: int, offset: tuple[int, int] = (0, 0), structureType=None, tags=[]):
-        self.position = position
-        self.length = length
-        self.width = width
-        self.offset = offset
-        self.structureType = structureType
-        self.tags = tags
+    def __init__(self, nbtName:str, level: int, position: tuple[int, int], doorPos: tuple[int, int], length: int, width: int, offset: tuple[int, int] = (0, 0), materialType=None, tags=[]):
+        self.nbtName = nbtName              # building name
+        self.level = level                  # building level: 1~3
+        self.position = position            # building coord
+        self.doorPos = doorPos              # door coord
+        self.length = length                # max length in this type of building
+        self.width = width                  # max width in this type of building
+        self.materialType = materialType    # building material
+        self.offset = offset                # ???
+        self.tags = tags                    # ???
