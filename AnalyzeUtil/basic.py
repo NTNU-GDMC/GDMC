4535c409c77b82d5d364bfd3e26630e8fbea9ba9
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2023-05-02 00:01:03 +0800

    feat: add analyze function `hasEnoughWood`
    TODO:
    After Subarya added the feature to access the resources inside a certain
    area, use that function to analyze this part

--- AnalyzeUtil/basic.py
+++ AnalyzeUtil/basic.py
@@ -3,9 +3,13 @@ from gdpc.geometry import Rect
 from math import sqrt
 
 
-MAXIMUM_SD= 5
-
+MAXIMUM_SD = 5
 def isFlat(core: Core, area: Rect, maxSD=MAXIMUM_SD) -> bool:
     """Only pick if the area's standard deviation is less than maxSD (more flat)"""
     return sqrt(core.getHeightMap("var", area)) <= maxSD
 
+MINIMUM_WOOD = 50 # TODO: Ask Subarya how many is enough
+def hasEnoughWood(core: Core, area: Rect, minWood=MINIMUM_WOOD):
+    """Choose if the wood in this area is above the threshold"""
+    # TODO: change the resource to a new method to get the resources in the area only
+    return core.resources.wood >= minWood 
