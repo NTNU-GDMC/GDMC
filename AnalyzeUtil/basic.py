3fb0f3aba87b48743cde979e25ce5adb2ca3718b
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2023-05-02 18:41:49 +0800

    feat: add analyze function `closeEnoughToRoad`

--- AnalyzeUtil/basic.py
+++ AnalyzeUtil/basic.py
@@ -9,7 +9,25 @@ def isFlat(core: Core, area: Rect, maxSD=MAXIMUM_SD) -> bool:
     return sqrt(core.getHeightMap("var", area)) <= maxSD
 
 MINIMUM_WOOD = 50 # TODO: Ask Subarya how many is enough
-def hasEnoughWood(core: Core, area: Rect, minWood=MINIMUM_WOOD):
+def hasEnoughWood(core: Core, area: Rect, minWood=MINIMUM_WOOD) -> bool:
     """Choose if the wood in this area is above the threshold"""
     # TODO: change the resource to a new method to get the resources in the area only
     return core.resources.wood >= minWood 
+
+MAXIMUM_ROAD_DISTANCE = 30
+def closeEnoughToRoad(core: Core, area:Rect, maxAverageDistance=MAXIMUM_ROAD_DISTANCE) -> bool:
+    """Check if the edge of the area is close enough to a road"""
+    x1, y1 = area.offset
+    xlen, ylen = area.size
+    x2 = x1 + xlen
+    y2 = y1 + ylen
+
+    for i in range(x1, x2):
+        if max(core.roadMap[i, y1], core.roadMap[i, y2 - 1]) <= maxAverageDistance:
+            return True
+
+    for i in range(y1, y2):
+        if max(core.roadMap[x1, i], core.roadMap[x2 - 1, i]) <= maxAverageDistance:
+            return True
+
+    return False
