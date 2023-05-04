b120b1a252472b000277d0293aff88558deef5f9
LoveSnowEx <lsktw22@gmail.com>
2023-05-04 05:39:57 +0000

    Refactor baseagent.py

--- classes/baseagent.py
+++ classes/baseagent.py
@@ -1,29 +1,13 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from .core import Core
 
 
-class Agent(object):
+class Agent(ABC):
     def __init__(self, core: Core) -> None:
         self.core = core
 
 
 class RunableAgent(Agent):
-    def __init__(self, core: Core) -> None:
-        super().__init__(core)
-
     @abstractmethod
     def run(self):
-        pass
-
-
-class Event():
-    pass
-
-
-class ObserverAgent(Agent):
-    def __init__(self, core: Core) -> None:
-        super().__init__(core)
-
-    @abstractmethod
-    def update(self, event: Event):
         pass
