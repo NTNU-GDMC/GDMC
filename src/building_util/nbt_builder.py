d51f40f2b98d82aa7f44ea70c15e57cbcd3e223e
LoveSnowEx <lsktw22@gmail.com>
2023-05-21 15:04:02 +0000

    Rename

--- src/building_util/nbt_builder.py
+++ src/building_util/nbt_builder.py
@@ -23,25 +23,25 @@ def getNBTAbsPath(name: str, type: int, level: int) -> str:
     return os.path.abspath(os.path.join(".", os.path.join("data", os.path.join("structures", os.path.join(name + f"{str(type)}", "level" + f"{str(level)}.nbt")))))
 
 
-def nbtToString(nbt_struct: nbt.TAG):
-    match nbt_struct:
+def nbtToString(struct: nbt.TAG):
+    match struct:
         case nbt.TAG_Compound():
             return '{{{}}}'.format(
-                ','.join(['{}:{}'.format(str(k), nbtToString(v)) for k, v in nbt_struct.iteritems()]))
+                ','.join(['{}:{}'.format(str(k), nbtToString(v)) for k, v in struct.iteritems()]))
         case nbt.TAG_List():
-            return '[{}]'.format(','.join([nbtToString(x) for x in nbt_struct]))
+            return '[{}]'.format(','.join([nbtToString(x) for x in struct]))
         case nbt.TAG_String():
-            return '"{}"'.format(str(nbt_struct))
+            return '"{}"'.format(str(struct))
         case nbt.TAG_Byte():
-            return '{}b'.format(str(nbt_struct))
+            return '{}b'.format(str(struct))
         case nbt.TAG_Float():
-            return '{}f'.format(str(nbt_struct))
+            return '{}f'.format(str(struct))
         case nbt.TAG_Double():
-            return '{}d'.format(str(nbt_struct))
+            return '{}d'.format(str(struct))
         case nbt.TAG_Int():
-            return '{}'.format(str(nbt_struct))
+            return '{}'.format(str(struct))
         case nbt.TAG_Long():
-            return '{}'.format(str(nbt_struct))
+            return '{}'.format(str(struct))
 
 
 def NBT2Blocks(struct: nbt.NBTFile, offset: ivec3 = ivec3(0, 0, 0)):
@@ -53,21 +53,21 @@ def NBT2Blocks(struct: nbt.NBTFile, offset: ivec3 = ivec3(0, 0, 0)):
         yield pos + offset, block
 
 
-def buildFromStructureNBT(editor: Editor, nbt_struct: nbt.NBTFile, offset: ivec3, biome: str = "", keep=False):
+def buildFromStructureNBT(editor: Editor, struct: nbt.NBTFile, offset: ivec3, biome: str = "", keep=False):
     if editor.worldSlice is None:
         raise Exception("Error while building structure: worldSlice is None")
 
     option = "keep" if keep else "replace"
 
     if not keep:
-        size = getStructureSizeNBT(nbt_struct)
+        size = getStructureSizeNBT(struct)
         bound = Box(offset, size)
         begin = bound.begin
         last = bound.last
         clearCmd = f"fill {begin.x} {begin.y} {begin.z} {last.x} {last.y} {last.z} barrier"
         editor.runCommand(clearCmd, syncWithBuffer=True)
 
-    for pos, block in NBT2Blocks(nbt_struct, offset):
+    for pos, block in NBT2Blocks(struct, offset):
         # FIXME: isChangeBlock and changeBlock function - SubaRya
         # if isChangeBlock(biome) == True:
         #     blkName = changeBlock(biome, blkName)
@@ -77,8 +77,8 @@ def buildFromStructureNBT(editor: Editor, nbt_struct: nbt.NBTFile, offset: ivec3
         editor.runCommand(cmd, syncWithBuffer=True)
 
 
-def getStructureSizeNBT(nbt_struct: nbt.NBTFile) -> ivec3:
-    size = nbt_struct["size"]
+def getStructureSizeNBT(struct: nbt.NBTFile) -> ivec3:
+    size = struct["size"]
     return ivec3(*map(lambda x: int(x.value), size))
 
 
