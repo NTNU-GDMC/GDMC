9a774ae3845cd95ec45e9b6cc9691145bc2723a4
LoveSnowEx <lsktw22@gmail.com>
2023-05-21 20:22:05 +0000

    Add comment to config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,29 @@
+"""
+This module contains the Config class for storing config data.
+You can save and load config data to and from a json file.
+Add new config data to the Config class and it will be able to save and load.
+
+Example:
+    Get the config data:
+        >>> from src.config.config import config
+        >>> buildArea = config.buildArea
+        >>> unit = config.unit
+
+    Modify the config class:
+        class Config:
+            buildArea: Box = Box((0, 0, 0), (255, 255, 255))
+            unit: int = 2
+
+            # You can add new config data here:
+            dataStructuresPath: Path = Path("data/structures")
+
+    Save the config data:
+        >>> config.save("custom_config.json")
+
+    Load the custom config data:
+        >>> config = Config.load("custom_config.json")
+"""
+
 import json
 from dataclasses import dataclass
 from pathlib import Path
@@ -16,6 +42,8 @@ class Config:
 
     # unit is the size of a single unit on the map
     unit: int = 2
+
+    #* You can add new config data here:
 
     def save(self, path: Path = DEFAULT_CONFIG_PATH):
         """Save config to a json file"""
