54bfd458cd44f96f319c24fe467f312dd52c097f
KutsunaSubaRya <index20010928@gmail.com>
2023-05-27 06:13:35 +0800

    feat: add building_limit

--- src/level/level_manager.py
+++ src/level/level_manager.py
@@ -12,47 +12,41 @@ from dataclasses import dataclass
 from pathlib import Path
 from ..resource.terrain_analyzer import Resource
 
-levelData: list[Resource] = []
-maxLevel:int = 0
-
-def initLimitResource():
-    global maxLevel
-    with open("src/level/resource_limit.json", "r") as f:
-        data = json.load(f)
-        maxLevel = data["maxLevel"]
-        for i in range(maxLevel):
-            human = data["human"][i]
-            wood = data["wood"][i]
-            stone = data["stone"][i]
-            iron_ore = data["iron_ore"][i]
-            iron = data["iron"][i]
-            food = data["food"][i]
-            levelData.append(Resource(human, wood, stone, iron_ore, iron, food))
-
-def getLimitResource(level: int) -> Resource:
-    return levelData[level]
-
 @dataclass
 class LevelManager:
-    resource: Resource
-    level: int = 1
+    levelResourceData: list[Resource] = []
+    levelBuildingData: list[int] = []
+    maxLevel:int = 0
     def __init__(self):
-        initLimitResource()
-        self.resource = getLimitResource(self.level - 1)
-    def __str__(self):
-        return "maxLevel:" + str(maxLevel) + "Level:" + str(self.level) + " " + str(self.resource)
-    def getLimitResource(self) -> Resource: 
-        """ return the limit resource of current level """
-        return self.resource
-    def levelUp(self) -> bool: 
+        self.initLimitResource()
+        self.initLimitBuilding()
+    def initLimitResource(self):
+        global maxLevel
+        with open("src/level/resource_limit.json", "r") as f:
+            data = json.load(f)
+            maxLevel = data["maxLevel"]
+            for i in range(maxLevel):
+                human = data["human"][i]
+                wood = data["wood"][i]
+                stone = data["stone"][i]
+                iron_ore = data["iron_ore"][i]
+                iron = data["iron"][i]
+                food = data["food"][i]
+                self.levelResourceData.append(Resource(human, wood, stone, iron_ore, iron, food))
+    def initLimitBuilding(self):
+        with open("src/level/building_limit.json", "r") as f:
+            data = json.load(f)
+            for i in range(maxLevel):
+                self.levelBuildingData.append(data["building_limit"][i])
+    def islevelUp(level:int) -> bool: 
         """ return true if level up successfully, else false """
-        if self.level ==  maxLevel:
+        if level ==  maxLevel:
             return False
-        self.level += 1
-        self.resource = getLimitResource(self.level - 1)
         return True
-    def getLevel(self) -> int:
-        return self.level
+    def getLimitResource(self, level: int) -> Resource:
+        return self.levelResourceData[level]
+    def getLimitBuilding(self, level: int) -> int:
+        return self.levelBuildingData[level]
 
 if __name__ == "__main__":
     levelManger = LevelManager()
