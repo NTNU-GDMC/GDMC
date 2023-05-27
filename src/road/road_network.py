0d1608506c3cb7acbd478f47586130aafdb3be4e
LoveSnowEx <lsktw22@gmail.com>
2023-05-27 12:46:37 +0000

    Refactor road_network.py

--- src/road/road_network.py
+++ src/road/road_network.py
@@ -1,62 +1,198 @@
-class RoadNode(object):
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+from typing import Generic, TypeVar, Callable
+
+T = TypeVar('T')
+
+
+class RoadNode(Generic[T]):
+    def __init__(self, val: T, hashfunc: Callable[[object], int] = hash):
+        self._val = val
+        self._hashfunc = hashfunc
 
     def __str__(self) -> str:
-        return f"({self.x}, {self.y})"
+        return f"RoadNode({self.val})"
 
     def __repr__(self) -> str:
         return self.__str__()
 
+    def __hash__(self) -> int:
+        return self._hashfunc(self.val)
 
-class RoadEdge(object):
-    def __init__(self, path: list[tuple[int, int]] = []):
-        self.path = [RoadNode(x, y) for x, y in path]
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, RoadNode):
+            return False
+        return self.val == o.val
 
-    def Node1(self):
-        return self.path[0]
+    @property
+    def val(self):
+        return self._val
 
-    def Node2(self):
-        return self.path[-1]
 
-    def len(self):
-        return len(self.path)
+class RoadEdge(Generic[T]):
+    def __init__(self, path: list[RoadNode[T]] = []):
+        self._path = [node for node in path]
+        self._nodes = set(self._path)
 
     def __str__(self) -> str:
-        return str(self.path)
+        return str(self._path)
 
     def __repr__(self) -> str:
         return self.__str__()
 
+    def __len__(self) -> int:
+        return len(self._path)
 
-class RoadNetwork(object):
-    def __init__(self, nodes: list[RoadNode], edges: list[RoadEdge]):
-        self.nodes = nodes
-        self.edges = edges
+    def __hash__(self) -> int:
+        return hash((self.node1, self.node2))
 
-    def getEdge(self, node1, node2):
-        for edge in self.edges:
-            if edge.Node1() == node1 and edge.Node2() == node2:
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, RoadEdge):
+            return False
+        return self.node1 == o.node1 and self.node2 == o.node2
+
+    def __contains__(self, node: RoadNode[T]) -> bool:
+        return node in self._nodes
+
+    @property
+    def path(self) -> list[RoadNode[T]]:
+        return self._path
+
+    @property
+    def node1(self) -> RoadNode[T]:
+        return self._path[0]
+
+    @property
+    def node2(self) -> RoadNode[T]:
+        return self._path[-1]
+
+    def split(self, node) -> tuple['RoadEdge[T]', 'RoadEdge[T]']:
+        idx = self._path.index(node)
+        return RoadEdge(self._path[:idx+1]), RoadEdge(self._path[idx:])
+
+    def reverse(self) -> 'RoadEdge[T]':
+        return RoadEdge(self._path[::-1])
+
+
+class RoadNetwork(Generic[T]):
+    def __init__(self, hotThreshold: int = 10):
+        """A graph of RoadNodes and RoadEdges. The graph is undirected, and edges are bidirectional."""
+        self._adj = dict[RoadNode[T], set[RoadEdge[T]]]()
+        self._hotness = dict[RoadNode[T], int]()
+        self._hotThreshold = hotThreshold
+
+    def __str__(self) -> str:
+        return str(self._adj)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    @property
+    def nodes(self):
+        """Returns an iterator over all main nodes in the graph."""
+        yield from self._adj.keys()
+
+    @property
+    def hotspots(self):
+        """Returns an iterator over all hotspots in the graph. Hotspots are nodes with a hotness above the hotThreshold."""
+        for node in self._hotness:
+            if self.isHotspot(node):
+                yield node
+
+    def hotness(self, node: RoadNode[T]) -> int:
+        """Returns the hotness of a node. Hotness is the number of edges that contain the node."""
+        return self._hotness[node]
+
+    def isHotspot(self, node: RoadNode[T]) -> bool:
+        """Returns whether a node is a hotspot."""
+        return self._hotness[node] >= self._hotThreshold
+
+    def updateHotspots(self):
+        """Updates the hotspots in the graph."""
+        edges = set(self.edges())
+        toUpgrade = set()
+        toDowngrade = set()
+
+        # Find nodes to upgrade and downgrade
+        for node in self._hotness:
+            if self.isHotspot(node) and node not in self._adj:
+                toUpgrade.add(node)
+            elif not self.isHotspot(node) and node in self._adj:
+                toDowngrade.add(node)
+
+        # upgrade nodes
+        for node in toUpgrade:
+            for edge in edges:
+                if node in edge:
+                    self.removeEdge(edge, updateHotspots=False)
+                    edge1, edge2 = edge.split(node)
+                    self.addEdge(edge1, updateHotspots=False)
+                    self.addEdge(edge2, updateHotspots=False)
+
+        # downgrade nodes
+        for node in toDowngrade:
+            self.removeNode(node, updateHotspots=False)
+
+    def addNode(self, node: RoadNode[T]):
+        """Adds a node to the graph."""
+        self._adj.setdefault(node, set())
+
+    def addEdge(self, edge: RoadEdge[T], updateHotspots: bool = True):
+        """Adds an edge to the graph."""
+        self._adj.setdefault(edge.node1, set()).add(edge)
+        self._adj.setdefault(edge.node2, set()).add(edge.reverse())
+
+        # update hotness
+        for node in edge.path:
+            self._hotness.setdefault(node, 0)
+            self._hotness[node] += 1
+
+        if updateHotspots:
+            self.updateHotspots()
+
+    def removeNode(self, node: RoadNode[T], updateHotspots: bool = True):
+        """Removes a node from the graph."""
+        if node not in self._adj:
+            return
+
+        for edge in self._adj[node]:
+            self._adj[edge.node1].remove(edge)
+            self._adj[edge.node2].remove(edge.reverse())
+
+            # update hotness
+            for node in edge.path:
+                self._hotness[node] -= 1
+
+        del self._adj[node]
+
+        if updateHotspots:
+            self.updateHotspots()
+
+    def removeEdge(self, edge: RoadEdge[T], updateHotspots: bool = True):
+        """Removes an edge from the graph."""
+        if edge.node1 not in self._adj[edge.node1] or edge.node2 not in self._adj[edge.node2]:
+            return
+
+        self._adj[edge.node1].remove(edge)
+        self._adj[edge.node2].remove(edge.reverse())
+
+        # update hotness
+        for node in edge.path:
+            self._hotness[node] -= 1
+
+        if updateHotspots:
+            self.updateHotspots()
+
+    def neighbors(self, node: RoadNode[T]):
+        """Returns an iterator over all neighbors of a node."""
+        yield from (edge.node2 for edge in self._adj[node])
+
+    def edge(self, node1: RoadNode[T], node2: RoadNode[T]) -> RoadEdge[T] | None:
+        """Returns the edge between two nodes, or None if there is no edge between them."""
+        for edge in self._adj[node1]:
+            if edge.node2 == node2:
                 return edge
         return None
 
-    def getEdgesByNode(self, node):
-        for edge in self.edges:
-            if edge.Node1() == node or edge.Node2() == node:
-                yield edge
-
-    def getEdgesByNodes(self, node1, node2):
-        for edge in self.edges:
-            if edge.Node1() == node1 and edge.Node2() == node2:
-                yield edge
-
-    def getEdgesByNode1(self, node1):
-        for edge in self.edges:
-            if edge.Node1() == node1:
-                yield edge
-
-    def getEdgesByNode2(self, node2):
-        for edge in self.edges:
-            if edge.Node2() == node2:
-                yield edge
+    def edges(self):
+        """Returns an iterator over all edges in the graph."""
+        for node in self._adj:
+            yield from self._adj[node]
