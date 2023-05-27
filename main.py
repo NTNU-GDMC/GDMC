8bcfcee6abe61a1216b1895c584b577a4608a524
KutsunaSubaRya <index20010928@gmail.com>
2023-05-27 10:25:00 +0800

    chore(main.py): add level manager README

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+"""
+Use islevelup = levelManager.isLevelUp(core.level, core.resources, len(core.blueprintData))
+    to ask levelManager can level up or not
+    if level up successfully, it means that:
+        1. level is not maxLevel
+        2. all resources are enough in this level
+        3. number of building is enough in this level
+
+Call core to level up:
+    core.levelUp(levelManager.getLimitResource(core.level), levelManager.getLimitBuilding(core.level))
+
+Use levelManager.getMostLackResource(...), (return value type is str)
+    to get the resource name(str) that is most shortage
+    this function CAN/MAYBE used to decide which resource should be gathered by agents
+
+Use levelManager.isLackBuilding(...), (return value type is bool)
+    to check if building is lack,namely, existBuilding < limitBuilding
+    this function CAN/MAYBE used to decide if agent should build building in this level or not
+"""
+
 # ! /usr/bin/python3
 from nbt import nbt
 from src.classes.core import Core
