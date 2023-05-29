28c6761c8e1e5deb45817310f07ca8223845306d
LoveSnowEx <lsktw22@gmail.com>
2023-05-29 17:21:11 +0000

    Refator agent generator

--- src/classes/agent_generator.py
+++ src/classes/agent_generator.py
@@ -1,4 +1,5 @@
 from typing import Callable
+from gdpc.vector_tools import Rect
 from ..classes.core import Core
 from ..classes.agent import RunableAgent
 from ..classes.agent import BuildAgent
@@ -10,6 +11,11 @@ from ..analyze_util.basic import isFlat, hasEnoughWood, closeEnoughToRoad, isLiq
 CHALET = "chalet"
 DESERT_BUILDING = "desert_building"
 
+BASIC_BUILDINGS = {
+    CHALET,
+    DESERT_BUILDING
+}
+
 # Special buildings
 
 SAWMILL = "sawmill"
@@ -18,48 +24,59 @@ QUARRY = "quarry"
 FORGE = "forge"
 CHURCH = "church"
 
-def newChaleteAgent(core: Core):
-    # Obviously fun(X
-    def analFun(c, a):
-        if isLiquid(c, a) or isDesert(c, a):
+SPECIAL_BUILDINGS = {
+    SAWMILL,
+    FARM,
+    QUARRY,
+    FORGE,
+    CHURCH
+}
+
+# Building tags
+
+TAG_LAND = "land"
+TAG_DESERT = "desert"
+TAG_CITY = "city"
+TAG_FOREST = "forest"
+
+BUILDING_TAGS = {
+    CHALET: [TAG_LAND],
+    DESERT_BUILDING: [TAG_LAND, TAG_DESERT],
+    SAWMILL: [TAG_LAND],
+    FARM: [TAG_LAND],
+    QUARRY: [TAG_LAND],
+    FORGE: [TAG_LAND],
+    CHURCH: [TAG_LAND, TAG_CITY]
+}
+
+
+def newAgent(core: Core, name: str):
+    tags = BUILDING_TAGS[name]
+
+    def analyzeFunction(core: Core, area: Rect):
+        total = 0
+
+        if TAG_LAND in tags and isLiquid(core, area):
             return 0
-        return isFlat(c, a) 
-    return BuildAgent(core, analFun, CHALET)
-
-
-def newDesertBuildingAgent(core: Core):
-    def analFun(c, a):
-        if isLiquid(c, a) or not isDesert(c, a):
+        if TAG_DESERT in tags and not isDesert(core, area):
             return 0
-        return isFlat(c, a)
-    return BuildAgent(core, analFun, DESERT_BUILDING)
-
-
-def newSawmillAgent(core: Core):
-    if isLiquid(c, a):
+        if TAG_DESERT not in tags and isDesert(core, area):
             return 0
-    def analyzeFunction(c, a): return isFlat(c, a) + hasEnoughWood(c, a) * 5
-    return BuildAgent(core, analyzeFunction, HUGE_SAWMILL)
 
+        flatness = isFlat(core, area)
+        if flatness == 0:
+            return 0
+        else:
+            total += flatness
+        if TAG_FOREST:
+            total += hasEnoughWood(core, area)
+        return total
 
-def placeholder(core: Core):
-    def analyzeFunction(c, a): return isFlat(c, a) + hasEnoughWood(c, a) * 5
-    return BuildAgent(core, analyzeFunction, HUGE_SAWMILL)
+    return BuildAgent(core, analyzeFunction, name)
 
 
 RunableAgentGenerator = Callable[[Core], RunableAgent]
 
 RUNABLE_AGENT_TABLE: dict[str, RunableAgentGenerator] = {
-    CHALET: newChaleteAgent,
-    DESERT_BUILDING: newDesertBuildingAgent,
-    SAWMILL: newSawmillAgent
-}
-
-# TODO: add real agent after the building is completed
-UNLOCKABLE_AGENT_TABLE: dict[str, RunableAgentGenerator] = {
-    "sawmill": placeholder,
-    "farm": placeholder,
-    "quarry": placeholder,
-    "forge": placeholder,
-    "church": placeholder,
+    name: lambda core: newAgent(core, name) for name in BASIC_BUILDINGS | SPECIAL_BUILDINGS
 }
